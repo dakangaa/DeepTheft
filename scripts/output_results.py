2636578fe07cfe41c474df2a025df871f6bc0030
dacom_Ubuntu <6dacom@gmail.com>
2025-03-15 06:03:05 +0800

    测试完成

--- scripts/output_results.py
+++ scripts/output_results.py
@@ -6,13 +6,13 @@ import re
 import argparse
 
 
-def test(HyperParameters, Origin_domain_nums, test_domain, args):
+def test(HyperParameters, origin_domain_nums, test_domain, args):
 
     path = "results/MateModel_Hyper"
     if args.mode == "O":
-        var2 = Origin_domain_nums
+        var2 = origin_domain_nums
         indexes = pd.MultiIndex.from_product(
-            [HyperParameters, Origin_domain_nums],
+            [HyperParameters, origin_domain_nums],
             names=["HyperParameters", "Origin_domain_nums"]
         )
         const_var = test_domain[0]
@@ -22,7 +22,7 @@ def test(HyperParameters, Origin_domain_nums, test_domain, args):
             [HyperParameters, test_domain],
             names=["HyperParameters", "test_domain"]
         )
-        const_var = Origin_domain_nums[0]
+        const_var = origin_domain_nums[0]
 
     columns = ["VAL_ACC", "VAL_F1", "TEST_ACC", "TEST_F1"]
     df = pd.DataFrame(np.zeros((len(HyperParameters) * len(var2), 4)), index=indexes, columns=columns)
@@ -35,7 +35,7 @@ def test(HyperParameters, Origin_domain_nums, test_domain, args):
             if args.mode == "O":
                 file = path + "/" + hp + "_" + str(v) + "_" + const_var + "_train_ckpt.pth"
                 test_cmd = ["python", "Step2_Layer-wise_Hyperparameter_Inferring/test.py",
-                            "-H", hp, "-o", str(v), "--device", args.device, "--test_domain", args.test_domain, "--workers", "3"]
+                            "-H", hp, "-o", str(v), "--device", args.device, "--test_domain", const_var, "--workers", "3"]
             elif args.mode == "T":
                 file = path + "/" + hp + "_" + str(const_var) + "_" + v + "_train_ckpt.pth"
                 test_cmd = ["python", "Step2_Layer-wise_Hyperparameter_Inferring/test.py",
@@ -53,57 +53,80 @@ def test(HyperParameters, Origin_domain_nums, test_domain, args):
 
     return df
 
-def read_epoch(HyperParameters, Origin_domain_nums, columns):
+def read_ckpt(hyperParameters, origin_domain_nums, test_domain, columns):
     path = "results/MateModel_Hyper"
+
     indexes = pd.MultiIndex.from_product(
-        [HyperParameters, Origin_domain_nums],
+        [hyperParameters, origin_domain_nums],
         names=["HyperParameters", "Origin_domain_nums"]
     )
-    df = pd.DataFrame(np.zeros((len(HyperParameters) * len(Origin_domain_nums), len(columns))), index=indexes, columns=columns)
-    for hp in HyperParameters:
-        for od in Origin_domain_nums:
+    df_od = pd.DataFrame(np.zeros((len(hyperParameters) * len(origin_domain_nums), len(columns))), index=indexes, columns=columns)
+    indexes = pd.MultiIndex.from_product(
+        [hyperParameters, test_domain],
+        names=["HyperParameters", "test_domain"]
+    )
+    df_td = pd.DataFrame(np.zeros((len(hyperParameters) * len(test_domain), len(columns))), index=indexes, columns=columns)
+    for hp in hyperParameters:
+        for od in origin_domain_nums:
             log = "HyperParameter:{}\t Origin_domain_nums:{}\t \nloading checkpoint..."
             print(log.format(hp, od))
-            file = path + "/" + hp + "_" + str(od) + "_train_ckpt.pth"
+            file = path + '/' + hp + "_" + str(od) + "_" + "331" + "_" + "train" + '_ckpt.pth'
             checkpoint = torch.load(file, map_location=torch.device('cpu'))
             for col in columns:
                 if col not in checkpoint.keys():
-                    df.loc[(hp, od), col] = float("nan")
+                    df_od.loc[(hp, od), col] = float("nan")
                     continue
                 if col in ["acc", "f1", "loss_value"]:
-                    df.loc[(hp, od), col] = checkpoint[col][0]
+                    df_od.loc[(hp, od), col] = checkpoint[col][0]
                 else:
-                    df.loc[(hp, od), col] = checkpoint[col]
-    return df
+                    df_od.loc[(hp, od), col] = checkpoint[col]
+        for td in test_domain:
+            file = path + '/' + hp + "_" + str(4) + "_" + td + "_" + "train" + '_ckpt.pth'
+            checkpoint = torch.load(file, map_location=torch.device('cpu'))
+            for col in columns:
+                if col not in checkpoint.keys():
+                    df_td.loc[(hp, td), col] = float("nan")
+                    continue
+                if col in ["acc", "f1", "loss_value"]:
+                    df_td.loc[(hp, td), col] = checkpoint[col][0]
+                else:
+                    df_td.loc[(hp, td), col] = checkpoint[col]
+    return df_od, df_td
 
 if __name__ == "__main__":
     # read epoch
-    # HyperParameters = ["kernel_size", "out_channels", "stride"]
-    # Origin_domain_nums = [1,2,3,4]
-    # columns = ["epoch", "acc", "f1", "loss_value"]
-    # df = read_epoch(HyperParameters, Origin_domain_nums, columns)
-    # print(df)
+    HyperParameters = ["kernel_size", "out_channels", "stride"]
+    Origin_domain_nums = [1,2,3,4]
+    columns = ["epoch", "loss_value"]
+    test_domain = ["160", "192", "224", "299", "331"]
+    df_od, df_td = read_ckpt(HyperParameters, Origin_domain_nums, test_domain, columns)
+    print(df_od)
+    print(df_td)
+    with pd.ExcelWriter("results/results.xlsx", if_sheet_exists="replace", mode="a") as writer:
+        df_od.to_excel(writer, sheet_name="O")
+    with pd.ExcelWriter("results/results.xlsx", if_sheet_exists="replace", mode="a") as writer:
+        df_td.to_excel(writer, sheet_name="T")
 
     # test
-    parser = argparse.ArgumentParser(description='collect data')
-    parser.add_argument("--device", type=str, default="autodl", help="laptop or autodl")
-    parser.add_argument("--mode", type=str, default="O", help="T est_domain or O rigin_domain_nums")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='collect data')
+    # parser.add_argument("--device", type=str, default="autodl", help="laptop or autodl")
+    # parser.add_argument("--mode", type=str, default="O", help="T est_domain or O rigin_domain_nums")
+    # args = parser.parse_args()
 
-    HyperParameters = ["kernel_size", "out_channels", "stride"]
-    if args.mode == "O":
-        origin_domain_nums = [1,2,3,4]
-        test_domain = ["331"]
-        df = test(HyperParameters, origin_domain_nums, test_domain, args)
-        print(df)
-        with pd.ExcelWriter("results/results.xlsx", if_sheet_exists="replace", mode="a") as writer:
-            df.to_excel(writer, sheet_name="origin_domain_num")
-    elif args.mode == "T":
-        origin_domain_nums = [4]
-        test_domain = ["160", "192", "224", "299", "331"]
-        df = test(HyperParameters, origin_domain_nums, test_domain, args)
-        print(df)
-        with pd.ExcelWriter("results/results.xlsx", if_sheet_exists="replace", mode="a") as writer:
-            df.to_excel(writer, sheet_name="test_domain")
-    else:
-        raise ValueError
+    # HyperParameters = ["kernel_size", "out_channels", "stride"]
+    # if args.mode == "O":
+    #     origin_domain_nums = [1,2,3,4]
+    #     test_domain = ["331"]
+    #     df = test(HyperParameters, origin_domain_nums, test_domain, args)
+    #     print(df)
+    #     with pd.ExcelWriter("results/results.xlsx", if_sheet_exists="replace", mode="a") as writer:
+    #         df.to_excel(writer, sheet_name="origin_domain_num")
+    # elif args.mode == "T":
+    #     origin_domain_nums = [4]
+    #     test_domain = ["160", "192", "224", "299", "331"]
+    #     df = test(HyperParameters, origin_domain_nums, test_domain, args)
+    #     print(df)
+    #     with pd.ExcelWriter("results/results.xlsx", if_sheet_exists="replace", mode="a") as writer:
+    #         df.to_excel(writer, sheet_name="test_domain")
+    # else:
+    #     raise ValueError
